40b1f0dd999a4a1d5a761150edf968e6676578cd
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-06-05 05:17:16 +0900

    Reformat 'person.Professor'

--- javaproject2-library-management/java/korean/librarymanagement/person/Professor.java
+++ javaproject2-library-management/java/korean/librarymanagement/person/Professor.java
@@ -1,7 +1,7 @@
 package java.korean.librarymanagement.person;
 
 public class Professor extends Person {
-	public Professor (int id, String name) {
+	public Professor(int id, String name) {
 		super(40, 120, id, name);
 	}
 }
