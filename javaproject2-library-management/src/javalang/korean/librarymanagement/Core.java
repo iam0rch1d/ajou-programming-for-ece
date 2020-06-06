23bb96e6c332c6d2c493299ec6f68f5788282b3d
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-06-06 09:36:00 +0900

    Refactor 'Core'

--- javaproject2-library-management/src/javalang/korean/librarymanagement/Core.java
+++ javaproject2-library-management/src/javalang/korean/librarymanagement/Core.java
@@ -2,14 +2,16 @@ package javalang.korean.librarymanagement;
 
 import java.util.Scanner;
 import javalang.korean.librarymanagement.collection.*;
+import javalang.korean.librarymanagement.person.Person;
 import javalang.korean.librarymanagement.person.PersonException;
+import javalang.korean.librarymanagement.person.Professor;
+import javalang.korean.librarymanagement.person.Student;
 
 public class Core {
     private static final Scanner scanner = new Scanner(System.in);
+    static Library library = new Library();
 
     public static void main(String[] args) {
-        Library library = new Library();
-
         while (true) {
             System.out.println("---------------- 아주대학교 도서관 관리 페이지입니다 --------------");
             System.out.println("1. 회원 등록");
@@ -27,31 +29,32 @@ public class Core {
                 int menuSelection = Integer.parseInt(scanner.nextLine());
 
                 switch (menuSelection) {
-                    case 1 -> library.addPerson(library.runCreatePersonInformationUi());
+                    case 1 -> library.addPerson(runCreatePersonInformationUi());
                     case 2 -> library.printPersonArrayList();
-                    case 3 -> library.addCollection(library.runCreateCollectionInformationUi());
+                    case 3 -> library.addCollection(runCreateCollectionInformationUi());
                     case 4 -> library.printCollectionArrayList();
                     case 5 -> library.saveLibrary();
                     case 6 -> {
-                        System.out.println("대출할 자료의 제목을 입력하세요 : ");
+                        System.out.print("대출할 자료의 제목을 입력하세요: ");
 
                         String title = scanner.nextLine();
 
                         System.out.println("자료를 대출하려는 회원을 검색합니다. 메뉴를 선택하십시오. ([1] 또는 [2])");
-                        System.out.println("([1] - 이름으로 검색, [2] - 고유번호(UID)로 검색)");
+                        System.out.print("([1] - 이름으로 검색, [2] - 고유번호(UID)로 검색): ");
 
                         try {
                             int submenuSelection = Integer.parseInt(scanner.nextLine());
 
                             switch (submenuSelection) {
-                                case 1 -> {
-                                    System.out.println("대출할 사용자의 이름을 입력하세요: ");
-                                    String name = scanner.nextLine();
-                                    library.borrowCollection(title, name);
+                                case 1 : {
+                                    System.out.print("자료를 대출할 사용자의 이름을 입력하세요: ");
+
+                                    String name = library.searchByName(scanner.nextLine());
                                 }
-                                case 2 -> {
-                                    System.out.println("대출할 사용자의 고유번호를 입력하세요: ");
+                                case 2 : {
+                                    System.out.print("자료를 대출할 사용자의 고유번호를 입력하세요: ");
                                     int id = Integer.parseInt(scanner.nextLine());
+
                                     library.borrowCollection(title, id);
                                 }
                             }
@@ -95,6 +98,95 @@ public class Core {
                     exception.getMessage() + " (" + exception.getClass().getName() + ")"
                 );
             }
+        }
+    }
+
+    private static Person runCreatePersonInformationUi() throws PersonException {
+        try {
+            System.out.print("회원의 고유번호(UID)를 입력하십시오.: ");
+            int uid;
+
+            while (true) {
+                uid = Integer.parseInt(scanner.nextLine());
+
+                if (library.isUidUnique(uid)) {
+                    break;
+                } else {
+                    System.out.print("중복된 고유번호(UID)가 존재합니다. 다시 입력해 주십시오.: ");
+                }
+            }
+
+            System.out.print("회원의 이름을 입력하십시오.: ");
+
+            String name = scanner.nextLine();
+
+            System.out.println("회원의 타입을 입력하십시오.");
+            System.out.print("([s] 또는 [Student] - Student / [p] 또는 [Professor] - Professor): ");
+
+            String className = scanner.nextLine();
+
+            if (className.toLowerCase().equals("s")
+                || className.toLowerCase().equals("student")) {
+                className = "Student";
+            } else if (className.toLowerCase().equals("p")
+                || className.toLowerCase().equals("professor")) {
+                className = "Professor";
+            } else {
+                throw new PersonException("에러: 회원 타입의 입력이 올바르지 않습니다.");
+            }
+
+            System.out.println("생성할 회원의 정보는 다음과 같습니다.");
+            System.out.println("고유번호(UID): " + uid);
+            System.out.println("이름: " + name);
+            System.out.println("타입: " + className);
+
+            if (className.equals("Student")) {
+                return new Student(uid, name);
+            } else {
+                return new Professor(uid, name);
+            }
+        } catch (NumberFormatException numberFormatException) {
+            System.out.println(
+                "에러: 숫자 형태의 고유번호(UID)를 입력해 주십시오. (" + numberFormatException.getClass().getName() + ")"
+            );
+        }
+
+        return null;
+    }
+
+    private static Collection runCreateCollectionInformationUi() throws CollectionException {
+        System.out.print("자료의 제목을 입력하십시오.: ");
+
+        String title = scanner.nextLine();
+
+        System.out.print("자료의 저자를 입력하십시오.: ");
+
+        String author = scanner.nextLine();
+
+        System.out.println("자료의 타입을 입력하십시오.");
+        System.out.print("([b] 또는 [Book] - Book / [c] 또는 [ClassMaterial] - ClassMaterial): ");
+
+        String className = scanner.nextLine();
+
+        if (className.toLowerCase().equals("b")
+            || className.toLowerCase().equals("book")) {
+            className = "Book";
+        } else if (className.toLowerCase().equals("c")
+            || className.toLowerCase().equals("classmaterial")) {
+            className = "ClassMaterial";
+        } else {
+            throw new CollectionException("에러: 자료 타입의 입력이 올바르지 않습니다.");
+        }
+
+        System.out.println("생성할 자료의 정보는 다음과 같습니다.");
+        System.out.println("제목: " + title);
+        System.out.println("저자: " + author);
+        System.out.println("타입: " + className);
+
+        if (className.equals("Book")) {
+            return new Book(title, author);
+        } else {
+            return new ClassMaterial(title, author);
         }
     }
 }
--- javaproject2-library-management/src/javalang/korean/librarymanagement/Library.java
+++ javaproject2-library-management/src/javalang/korean/librarymanagement/Library.java
@@ -6,7 +6,6 @@ import javalang.korean.librarymanagement.person.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Library {
 	static final int PERSON_UID = 0;
@@ -16,7 +15,6 @@ public class Library {
 	static final int COLLECTION_AUTHOR = 1;
 	static final int COLLECTION_CLASSNAME = 2;
 
-	private static final Scanner scanner = new Scanner(System.in);
 	private final ArrayList<Person> personArrayList;
 	private final ArrayList<Collection> collectionArrayList;
 
@@ -95,92 +93,22 @@ public class Library {
 		}
 	}
 
+	boolean isUidUnique(int uid) {
+		for (Person element : personArrayList) {
+			if (uid == element.getUid()) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 	void addPerson(Person person) {
 		personArrayList.add(person);
 	}
 
-	Person runCreatePersonInformationUi() throws PersonException {
-		System.out.print("회원의 고유번호(UID)를 입력하십시오.: ");
-
-		try {
-			int uid = Integer.parseInt(scanner.nextLine());
-
-			System.out.print("회원의 이름을 입력하십시오.: ");
-
-			String name = scanner.nextLine();
-
-			System.out.println("회원의 타입을 입력하십시오.");
-			System.out.print("([s] 또는 [Student] - Student / [p] 또는 [Professor] - Professor): ");
-
-			String className = scanner.nextLine();
-
-			if (className.toLowerCase().equals("s")
-				|| className.toLowerCase().equals("student")) {
-				className = "Student";
-			} else if (className.toLowerCase().equals("p")
-				|| className.toLowerCase().equals("professor")) {
-				className = "Professor";
-			} else {
-				throw new PersonException("에러: 회원 타입의 입력이 올바르지 않습니다.");
-			}
-
-			System.out.println("생성할 회원의 정보는 다음과 같습니다.");
-			System.out.println("고유번호(UID): " + uid);
-			System.out.println("이름: " + name);
-			System.out.println("타입: " + className);
-
-			if (className.equals("Student")) {
-				return new Student(uid, name);
-			} else {
-				return new Professor(uid, name);
-			}
-		} catch (NumberFormatException numberFormatException) {
-			System.out.println(
-				"에러: 숫자 형태의 고유번호(UID)를 입력해 주십시오. (" + numberFormatException.getClass().getName() + ")"
-			);
-		}
-
-		return null;
-	}
-
-	public void addCollection(Collection collection) {
+	void addCollection(Collection collection) {
 		collectionArrayList.add(collection);
-	}
-
-	Collection runCreateCollectionInformationUi() throws CollectionException {
-		System.out.print("자료의 제목을 입력하십시오.: ");
-
-		String title = scanner.nextLine();
-
-		System.out.print("자료의 저자를 입력하십시오.: ");
-
-		String author = scanner.nextLine();
-
-		System.out.println("자료의 타입을 입력하십시오.");
-		System.out.print("([b] 또는 [Book] - Book / [c] 또는 [ClassMaterial] - ClassMaterial): ");
-
-		String className = scanner.nextLine();
-
-		if (className.toLowerCase().equals("b")
-			|| className.toLowerCase().equals("book")) {
-			className = "Book";
-		} else if (className.toLowerCase().equals("c")
-			|| className.toLowerCase().equals("classmaterial")) {
-			className = "ClassMaterial";
-		} else {
-			throw new CollectionException("에러: 자료 타입의 입력이 올바르지 않습니다.");
-		}
-
-		System.out.println("생성할 자료의 정보는 다음과 같습니다.");
-		System.out.println("제목: " + title);
-		System.out.println("저자: " + author);
-		System.out.println("타입: " + className);
-
-		if (className.equals("Book")) {
-			return new Book(title, author);
-		} else {
-			return new ClassMaterial(title, author);
-		}
 	}
 
 	public void printPersonArrayList() {
@@ -234,6 +162,36 @@ public class Library {
 			bufferedWriter.close();
 		} catch (IOException ioException) {
 			System.out.println(Arrays.toString(ioException.getStackTrace()));
+		}
+	}
+
+	String searchByName(String name) throws PersonException {
+		int countPersonWithName = 0;
+
+		for (Person element : personArrayList) {
+			if (name.equals(element.getName())) {
+				countPersonWithName++;
+			}
+		}
+
+		switch (countPersonWithName) {
+			case 0 -> throw new PersonException("입력한 이름을 가진 회원의 검색 결과가 존재하지 않습니다.");
+			case 1 -> {
+				return name;
+			}
+			default -> {
+				System.out.println("입력한 이름을 가진 회원이 " + countPersonWithName + "건 검색되었습니다.");
+
+				for (Person element : personArrayList) {
+					if (name.equals(element.getName())) {
+						System.out.print(element.getUid() + " | ");
+						System.out.print(element.getName() + " | ");
+						System.out.println(element.getClass().getSimpleName());
+					}
+				}
+
+				throw new PersonException("uid로 검색해 주십시오.");
+			}
 		}
 	}
 
