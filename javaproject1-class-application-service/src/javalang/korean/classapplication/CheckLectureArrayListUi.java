7291c255652c483b1e49ad3a58b0c22aeb696b7c
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-06-20 22:02:45 +0900

    Refactor to method 'printLectureArrayList'

--- javaproject1-class-application-service/src/javalang/korean/classapplication/CheckLectureArrayListUi.java
+++ javaproject1-class-application-service/src/javalang/korean/classapplication/CheckLectureArrayListUi.java
@@ -8,14 +8,27 @@ import java.awt.event.ActionListener;
 
 class CheckLectureArrayListUi extends JPanel implements ActionListener {
 	private final Student student;
-	private int numRow;
 	private JButton refreshButton;
-
-	private final int MARGIN = 5;
-	private final int NUM_COLUMN = 5;
 
 	CheckLectureArrayListUi(Student student) {
 		this.student = student;
+
+		printLectureArrayList();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().equals(refreshButton)) {
+			removeAll();
+			printLectureArrayList();
+		}
+	}
+
+	private void printLectureArrayList() {
+		final int NUM_COLUMN = 5;
+		final int MARGIN = 5;
+
+		int numRow;
 
 		if (student.getLectureArrayList().isEmpty()) {
 			numRow = student.getLectureArrayList().size() + 3;
@@ -54,57 +67,7 @@ class CheckLectureArrayListUi extends JPanel implements ActionListener {
 					if (i == 1 && j == 2) {
 						add(new JLabel("등록된 강의가 없습니다."));
 					} else {
-						add(new JLabel(""));
-					}
-				}
-			}
-		}
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource().equals(refreshButton)) {
-			removeAll();
-
-			if (student.getLectureArrayList().isEmpty()) {
-				numRow = student.getLectureArrayList().size() + 3;
-			} else {
-				numRow = student.getLectureArrayList().size() + 2;
-			}
-
-			setBorder(new EmptyBorder(MARGIN, MARGIN, MARGIN, MARGIN));
-			setLayout(new GridLayout(numRow, NUM_COLUMN, MARGIN, MARGIN));
-
-			for (int i = 0; i < numRow; i++) {
-				for (int j = 0; j < NUM_COLUMN; j++) {
-					if (i == 0) {
-						JLabel[] legendLabel = new JLabel[NUM_COLUMN];
-						String[] legendText = {"No.", "과목명", "교수명", "시간", "수강가능학년"};
-						legendLabel[j] = new JLabel(legendText[j]);
-
-						legendLabel[j].setHorizontalAlignment(JLabel.CENTER);
-						add(legendLabel[j]);
-					} else if (i == numRow - 1) {
-						if (j == 0) {
-							refreshButton = new JButton("새로고침");
-
-							add(refreshButton);
-							refreshButton.addActionListener(this);
-						} else {
-							add(new JLabel());
-						}
-					} else if (student.getLectureArrayList().size() > 0) switch (j) {
-						case 0 -> add(new JLabel("" + i));
-						case 1 -> add(new JLabel(student.getLectureArrayList().get(i - 1).getName()));
-						case 2 -> add(new JLabel(student.getLectureArrayList().get(i - 1).getProfessorName()));
-						case 3 -> add(new JLabel(student.getLectureArrayList().get(i - 1).getTime()));
-						case 4 -> add(new JLabel("" + student.getLectureArrayList().get(i - 1).getMinimumGrade()));
-					} else if (student.getLectureArrayList().size() == 0) {
-						if (i == 1 && j == 2) {
-							add(new JLabel("등록된 강의가 없습니다."));
-						} else {
-							add(new JLabel(""));
-						}
+						add(new JLabel());
 					}
 				}
 			}
