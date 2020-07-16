48bac5cfb067158e2dec75751643ef5de27de4e0
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-07-16 16:59:49 +0900

    Reformat 'exercise1'

--- exercise1/part1.c
+++ exercise1/part1.c
@@ -1,63 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Macros
 #define MAX_TOWER_SIZE 64
 #define MIN_TOWER_SIZE 1
 
+enum boolean {
+    false = 0,
+    true
+};
+
 // Global variables
 unsigned long long countMove;
 
 // Function prototypes
-unsigned int setTowerSize(void);
 void moveHanoi(unsigned int, char, char, char);
 void moveDisk(unsigned int, char, char);
 
-//----------------------------------------------------------------------------------------------------------------------
 // Main function
 int main() {
-    moveHanoi(setTowerSize(), 'A', 'B', 'C');
+    int towerSize;
+    
+    while (true) {
+        printf("Input size of tower. [%d-%d]:", MIN_TOWER_SIZE, MAX_TOWER_SIZE);
+
+        char towerSizeString[3];
+
+        scanf("%s", towerSizeString);
+
+        towerSize = strtol(towerSizeString, NULL, 10);
+
+        // Limit tower size not to overflow at counting moves
+        if (towerSize < MIN_TOWER_SIZE || towerSize > MAX_TOWER_SIZE) {
+            printf("Invalid tower size. Please input again.\n");
+        } else break;
+    }
+
+    moveHanoi(towerSize, 'A', 'B', 'C');
 
     return 0;
 }
-//----------------------------------------------------------------------------------------------------------------------
+
 // Functions
-
-// setTowerSize() - Scan size of tower from user
-unsigned int setTowerSize(void) {
-    int towerSize;
-
-    while (1) {
-        printf("Input size of tower. [%d-%d]:", MIN_TOWER_SIZE, MAX_TOWER_SIZE);
-        scanf("%d", &towerSize);
-
-        if (towerSize < MIN_TOWER_SIZE || towerSize > MAX_TOWER_SIZE) { // Limit tower size not to overflow at counting
-                                                                        // moves
-            printf("Invalid tower size. Please input again.\n");
-        }
-        else {
-            return towerSize;
-        }
+/**
+ * void moveHanoi(unsigned int, char, char, char)
+ * Moves tower with size of [towerSize] by recursion.
+ */
+void moveHanoi(unsigned int towerSize, char sourcePeg, char auxiliaryPeg, char destinationPeg) {
+    if (towerSize == 1) { // Base case. Move single disk
+        moveDisk(towerSize, sourcePeg, destinationPeg);
+    } else { // Recursion steps. Move multiple disks
+        // Move all disks above the lowest disk from source peg to auxiliary peg, in order to move the lowest disk from
+        // source peg to destination peg
+        moveHanoi(towerSize - 1, sourcePeg, destinationPeg, auxiliaryPeg);
+        // Move the lowest disk from source peg to destination peg
+        moveDisk(towerSize, sourcePeg, destinationPeg);
+        // Move left disks and put on the lowest disk
+        moveHanoi(towerSize - 1, auxiliaryPeg, sourcePeg, destinationPeg);
     }
 }
 
-// moveHanoi() - Move whole tower by recursion
-void moveHanoi(unsigned int towerSize, char srcPeg, char auxPeg, char dstPeg) {
-    if (towerSize == 0) { // Exception. Does not occur
-        printf("Can't move.\n");
-    }
-    else if (towerSize == 1) { // Base case. Move single disk
-        moveDisk(towerSize, srcPeg, dstPeg);
-    }
-    else { // Recursion steps. Move multiple disk
-        moveHanoi(towerSize - 1, srcPeg, dstPeg, auxPeg); // Move all disks above lowest disk from source peg to
-                                                          // auxiliary peg, in order to move lowest disk from source peg
-                                                          // to destination peg
-        moveDisk(towerSize, srcPeg, dstPeg); // Move lowest disk from source peg to destination peg
-        moveHanoi(towerSize - 1, auxPeg, srcPeg, dstPeg); // Move left disks and put on lowest disk
-    }
-}
-
-// moveDisk() - Move single disk
-void moveDisk(unsigned int diskSize, char srcPeg, char dstPeg) {
-    printf("Move size %d disk from %c to %c. (Moves: %lld)\n", diskSize, srcPeg, dstPeg, ++countMove);
+/**
+ * void moveDisk(unsigned int, char, char)
+ * Moves single disk
+ */
+void moveDisk(unsigned int diskSize, char sourcePeg, char destinationPeg) {
+    printf("Move size %d disk from %c to %c. (Moves: %lld)\n", diskSize, sourcePeg, destinationPeg, ++countMove);
 }
--- exercise1/part2.c
+++ exercise1/part2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 // Macros
@@ -6,94 +7,69 @@
 #define FIRST_PRIME 2
 #define COMPOSITE 0
 
-// Global variables
-int range;
-int arrNumTable[MAX_RANGE + 1]; // Each index of number table array corresponds to the number
-                                // ('arrNumTable[0]' is not used)
+enum boolean {
+    false = 0,
+    true
+};
 
-// Function prototypes
-int setRange(void);
-void getArrNumTable(void);
-void sift(void);
-void printAllPrimesInRange(void);
-
-//----------------------------------------------------------------------------------------------------------------------
 // Main function
 int main() {
-    range = setRange();
-    getArrNumTable();
-    sift();
-    printAllPrimesInRange();
+    int numberArray[MAX_RANGE + 1];
+    int range;
 
-    return 0;
-}
-//----------------------------------------------------------------------------------------------------------------------
-// Functions
-
-// setRange() - Scan range from user
-int setRange(void) {
     while (1) {
+        char rangeString[10];
+
         printf("Input range. [1-%d]:", MAX_RANGE);
-        scanf("%d", &range);
+        scanf("%s", rangeString);
 
-        if (range < 1 || range > MAX_RANGE) { // Limit range not to exceed number table array
+        range = strtol(rangeString, NULL, 10);
+
+        if (range < 1 || range > MAX_RANGE) { // Limit range not to exceed number array
             printf("Invalid range. Please input again.\n");
-        }
-        else {
-            return range;
-        }
+        } else break;
     }
-}
 
-// getArrNumTable() - Make sifting number table
-void getArrNumTable(void) {
     int i;
 
     for (i = 0; i <= range; i++) {
-        arrNumTable[i] = i;
+        numberArray[i] = i;
     }
-}
 
-// sift() - Sift composite numbers in number table
-void sift(void) {
-    int i;
     int multiplier;
     int multiple;
 
-    arrNumTable[1] = COMPOSITE; // Number '1' is not prime number (exception)
+    numberArray[1] = COMPOSITE; // Number '1' is not prime number (exception)
 
     for (i = FIRST_PRIME; i <= sqrt(range); i++) {
         multiplier = i;
         multiple = i;
 
-        if (arrNumTable[multiplier] == COMPOSITE) { // Skip loop for composite number index
+        if (numberArray[multiplier] == COMPOSITE) { // Skip loops for composite number index
             continue;
-        }
-        else {
+        } else {
             while (multiple <= range) {
                 multiple += multiplier;
-                arrNumTable[multiple] = COMPOSITE; // Set composite number index element to 'COMPOSITE'(0)
+                numberArray[multiple] = COMPOSITE; // Set composite number index element to [COMPOSITE](0)
             }
         }
     }
-}
-
-// printAllPrimesInRange() - Print all prime numbers in range, print number of primes in range
-void printAllPrimesInRange(void) {
-    int countPrime = 0;
-    int i;
 
     printf("Primes in range of %d:\n", range);
 
+    int countPrime = 0;
+
     for (i = 0; i <= range; i++) {
-        if (arrNumTable[i] != COMPOSITE) {
+        if (numberArray[i] != COMPOSITE) {
             countPrime++;
 
-            printf("%d ", arrNumTable[i]);
+            printf("%d ", numberArray[i]);
         }
     }
 
     printf("\n");
     printf("Number of primes in range of %d:\n", range);
     printf("%d\n", countPrime);
+
+    return 0;
 }
